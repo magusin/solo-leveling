d7c662cdaf7bcc06b10a485820200257095a18e6
Hugo JACQUES <fanatsy4@gmail.com>
2024-12-02 13:25:04 +0100

    fix prisma dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Image from 'next/image';
-import Header from '@/components/Header';
-
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
